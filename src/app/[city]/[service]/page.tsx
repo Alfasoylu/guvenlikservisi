8f603e4bcf8cabac7c32e21812f69ef1b1b7c220
Alfasoylu <alfasoyluelektronik@gmail.com>
2026-03-09 01:47:36 +0300

    Set dynamicParams to false in page.tsx

--- src/app/[city]/[service]/page.tsx
+++ src/app/[city]/[service]/page.tsx
@@ -1,7 +1,7 @@
 import { cities } from "@/data/cities";
 import { services } from "@/data/services";
 import { notFound } from "next/navigation";
-
+export const dynamicParams = false;
 export function generateStaticParams() {
   const paths = [];
 
