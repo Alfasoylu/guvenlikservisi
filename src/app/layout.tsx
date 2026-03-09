223b9a378a38370e301de514e47eb76d6b5e0ebf
Alfasoylu <alfasoyluelektronik@gmail.com>
2026-03-09 19:18:12 +0300

    Add Google Search Console verification and fix GTM strategy

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -74,6 +74,9 @@ export const metadata: Metadata = {
     shortcut: "/favicon.ico",
     apple: "/favicon.ico",
   },
+  verification: {
+    google: "i5B6tYYGx0eggv9nie4ynlNytDsffEhyIfvVpkmA-K4",
+  },
 };
 
 export const viewport: Viewport = {
@@ -93,17 +96,17 @@ export default function RootLayout({
     <html lang="tr">
       <head>
         {GTM_ID && (
-          <Script id="gtm-script" strategy="afterInteractive">
+          <Script id="gtm-script" strategy="beforeInteractive">
             {`
-              (function(w,d,s,l,i){w[l]=w[l]||[];
-              w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});
-              var f=d.getElementsByTagName(s)[0],
-              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';
-              j.async=true;
-              j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
-              f.parentNode.insertBefore(j,f);
-              })(window,document,'script','dataLayer','${GTM_ID}');
-            `}
+  (function(w,d,s,l,i){w[l]=w[l]||[];
+  w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});
+  var f=d.getElementsByTagName(s)[0],
+  j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';
+  j.async=true;
+  j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
+  f.parentNode.insertBefore(j,f);
+  })(window,document,'script','dataLayer','${GTM_ID}');
+  `}
           </Script>
         )}
       </head>
